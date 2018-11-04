5b812bf4aa604a7546f68f41af57b8df1fb0c273
Don Coleman <dcoleman@chariotsolutions.com>
2018-11-04 09:28:26 -0800

    index

--- www/subscribe/index.js
+++ www/subscribe/index.js
@@ -1,18 +1,30 @@
 var client;
 
 function connect() {
+  console.log('Connecting');
   client = mqtt.connect('wss://mqtt.iotwork.shop:8083', {
     username: username.value,
     password: password.value 
   });
 
   client.on('connect', function () { 
+    pre.innerText = '';   
+    console.log('connected');
+    connectButton.hidden = true;
+    disconnectButton.hidden = false;
     client.subscribe(topic.value);
   })
 
   client.on('message', function (topic, message) {
+    // message is Buffer so call toString before logging
     console.log(topic, message.toString()); 
-    pre.innerText += '\n' + topic + '\t' + message;
+    pre.innerText += '\n' + new Date() + '\t' + topic + '\t' + message;
+    window.scrollTo(0,document.body.scrollHeight);
+  })
+
+  client.on('error', function (message) {
+      alert(message);
+      client.end();
   })
 }
 
